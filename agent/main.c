5e6fd4dfbd3b6ae75328610673122c29dc8c6aae
apriljade <seonggyu@kakao.com>
2022-08-01 17:30:57 +0900

    Update Makefile

--- agent/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(void)
-{
-	printf("Clock Tick: %ld\n", sysconf(_SC_CLK_TCK));
-	return 0;
-}
---
+++ agent/srcs/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#define SYSTEM_INFO "/proc/stat"
+#define PROCESS_INFO "/proc/%d/stat"
+#define MEMORY_INFO "/proc/meminfo"
+#define DISK_INFO "/proc/diskstats"
+
+int main(void)
+{
+	int fd;
+
+	if ((fd = open(SYSTEM_INFO, O_RDONLY)) == -1)
+	{
+		perror("agent: ");
+		exit(errno);
+	}
+	char buf[8192] = { 0, };
+	int size = read(fd, buf, 8192);
+	buf[size] = 0;
+	printf("%s\n", buf);
+	close(fd);
+	return 0;
+}
