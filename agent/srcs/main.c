2a89a48e788ad07e04117c673b8c856dcebc2187
apriljade <seonggyu@kakao.com>
2022-08-02 16:20:06 +0900

    Implement collectCpuInfo function

--- agent/srcs/main.c
+++ agent/srcs/main.c
@@ -14,41 +14,56 @@
 #define DISK_INFO		"/proc/diskstats"
 #define NET_INFO		"/proc/net/dev"
 
-int main(void)
+
+void	collectCpuInfo(int us, long cpuCnt, long timeConversion, char* rdBuf)
 {
 	int	fd;
-	char buf[SYS_INFO_BUF_SIZE + 1] = { 0, };
+	int readSize = 0;
+
+	fd = open("/proc/stat", O_RDONLY);
+	if (fd == -1)
+	{
+		// TODO: Handling open error
+		perror("agent");
+		return ;
+	}
+	readSize = read(fd, rdBuf, SYS_INFO_BUF_SIZE);
+	if (readSize != SYS_INFO_BUF_SIZE)
+	{
+		// TODO: Handling read error
+		perror("agent");
+		return ;
+	}
+	rdBuf[SYS_INFO_BUF_SIZE] = 0;
+	for (int i = 0; i < cpuCnt; i++)
+	{
+		printf("cpu: %d\t", i);
+		while (*rdBuf++ != ' ');
+		size_t userModeRunningTime = atoll(rdBuf) * timeConversion;
+		while (*rdBuf++ != ' ');
+		size_t sysModeRunningTime = atoll(rdBuf) * timeConversion;
+		while (*rdBuf++ != ' ');
+		while (*rdBuf++ != ' ');
+		size_t idleTime = atoll(rdBuf) * timeConversion;
+		while (*rdBuf++ != ' ');
+		size_t waitTime = atoll(rdBuf) * timeConversion;
+		printf("user: %8ld ms\tsys: %8ld ms\tidle: %8ld ms\twait: %10ld ms\n",
+			userModeRunningTime, sysModeRunningTime, idleTime, waitTime);
+		while (*rdBuf++ != '\n');
+	}
+	usleep(us);
+	close(fd);
+}
+
+int main(void)
+{
+	char	buf[SYS_INFO_BUF_SIZE] = { 0, };
 	long logicalCoreCount = sysconf(_SC_NPROCESSORS_ONLN);
 	long oneTick = sysconf(_SC_CLK_TCK);
 	long toMs = 1000 / oneTick;
-	int readSize = 0;
-	while (1)
+
+	while(1)
 	{
-		fd = open("/proc/stat", O_RDONLY);
-		if (fd == -1)
-			return 1;
-		readSize = read(fd, buf, SYS_INFO_BUF_SIZE);
-		if (readSize != SYS_INFO_BUF_SIZE)
-			return 1;
-		buf[SYS_INFO_BUF_SIZE] = 0;
-		char* pb = buf;
-		for (int i = 0; i < logicalCoreCount; i++)
-		{
-			printf("cpu: %d\t", i);
-			while (*pb++ != ' ');
-			size_t userModeRunningTime = atoll(pb) * toMs;
-			while (*pb++ != ' ');
-			size_t sysModeRunningTime = atoll(pb) * toMs;
-			while (*pb++ != ' ');
-			while (*pb++ != ' ');
-			size_t idleTime = atoll(pb) * toMs;
-			while (*pb++ != ' ');
-			size_t waitTime = atoll(pb) * toMs;
-			printf("user: %8ld ms\tsys: %8ld ms\tidle: %8ld ms\twait: %10ld ms\n",
-				userModeRunningTime, sysModeRunningTime, idleTime, waitTime);
-			while (*pb++ != '\n');
-		}
-		usleep(3000);
-		close(fd);
+		collectCpuInfo(2000, logicalCoreCount, toMs, buf);
 	}
 }
