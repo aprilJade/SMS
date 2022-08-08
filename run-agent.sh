e21e073ebb1df453ae4303d7c8d3b6d1a9ce6cdd
apriljade <seonggyu@kakao.com>
2022-08-08 23:49:17 +0900

    Update run-agent.sh

--- run-agent.sh
+++ run-agent.sh
@@ -1,5 +1,8 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/apriljade/Documents/repository/SMS/SMSutils
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/apriljade/Documents/repository/SMS/collector
+# If you want remove below 2 lines, you must register necessary librarys to standard library path.
+# Such as, /usr/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/SMSutils
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/collector
+
 # -p [period] -c [period] -m [period] -n [period]
 # The [period] is the collection period and is in milliseconds.
 # If you want collect CPU info every 500ms, memory info every 500ms, network info every 500ms and
