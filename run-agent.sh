db4f6ee3dfaf4c39e312591dbf71823751749a82
apriljade <seonggyu@kakao.com>
2022-08-22 18:08:36 +0900

    Fix run-agent

--- run-agent.sh
+++ run-agent.sh
@@ -8,4 +8,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/bin
 # process info every 3000ms, then start below command
 # ./agent/agent -c 500 -m 500 -n 500 -p 3000
 
-./bin/agent -d 3000 -H 127.0.0.1:4242
+./bin/agent
