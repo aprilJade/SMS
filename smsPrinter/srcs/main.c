b74b048603fb465eb8e41f76c51645927bcaeaf8
apriljade <seonggyu@kakao.com>
2022-09-07 11:37:53 +0900

    Update printer

--- smsPrinter/srcs/main.c
+++ smsPrinter/srcs/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/time.h>
 #include "libpq-fe.h"
 
 char* sql = "SELECT * FROM %s WHERE collect_time > \'%s\';";
@@ -26,6 +26,11 @@ int main(int argc, char** argv)
     sprintf(buf, "SELECT * FROM %s WHERE collect_time > \'%s\' AND collect_time < \'%s\';", argv[1], argv[2], argv[3]);
 
     printf("Querying below SQL.\n%s\n", buf);
+    
+    unsigned long prevTime;
+    struct timeval* tv;
+    gettimeofday(tv, NULL);
+    prevTime = tv->tv_sec * 1000000 + tv->tv_usec;
     PGresult* result = PQexec(pgConn, buf);
     
     if (PQresultStatus(result) != PGRES_TUPLES_OK)
@@ -42,15 +47,24 @@ int main(int argc, char** argv)
     int fieldsCnt = PQnfields(result);
     for (int i = 0; i < fieldsCnt; i++)
         printf("%-20s", PQfname(result, i));
-    putchar('\n');
 
-    for (int i = 0; i < PQntuples(result); i++)
+    putchar('\n');
+    for (int i = 0; i < fieldsCnt * 20; i++)
+        putchar('-');
+    putchar('\n');
+    
+
+    int tuplesCnt = PQntuples(result);
+    for (int i = 0; i < tuplesCnt; i++)
     {
         for (int j = 0; j < fieldsCnt; j++)
             printf("%-20s", PQgetvalue(result, i, j));
         putchar('\n');
     }
-    
+
+    gettimeofday(tv, NULL);
+    unsigned long elapsedTime = (tv->tv_sec * 1000000 + tv->tv_usec) - prevTime;   
+    printf("Total %d row in %.2f ms\n", tuplesCnt, elapsedTime / 1000.0);
     PQclear(result);
     return 0;
 }
